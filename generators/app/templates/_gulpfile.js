f0399badc978bd818cb888d1b8bad51b0b3c4b4d
Lingfeng Yang <hulk@juhuiwan.cn>
2016-06-07 20:38:36 +0800

    add stream reload for scss

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -82,7 +82,7 @@ gulp.task('web-server', ['build'],function() {
 
 gulp.task('watch', ['web-server'], function() {
   gulp.watch(config.script.watch, ['webpack']).on('change', reload);
-  gulp.watch(config.scss.src, ['webpack']).on('change', reload);
+  gulp.watch(config.scss.src, ['webpack']).on('change', reload({stream:true}));
   gulp.watch(config.src + '/**/*.vue', ['webpack']).on('change', reload);
   gulp.watch(config.html.watchHome, ['html']).on('change', reload);
   gulp.watch(config.html.watchAll, ['html']).on('change', reload);
